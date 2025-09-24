ec2f8a8aa8c3961deb696433d36a96c82b30e92d
NNichol-Python <nicholnathaniel@gmail.com>
2025-09-24 13:29:53 -0700

    addressing path issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/placeholderwebsite/',
   build: {
     outDir: 'docs', // <-- output build to docs/
   },
